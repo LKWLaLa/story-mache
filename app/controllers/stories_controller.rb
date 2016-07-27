60c4ba9b508b050a0a42f6cab5327ece27880348
LKWLaLa <lkwlala@gmail.com>
2016-07-26 21:31:59 -0400

    add actions for stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,35 @@
 class StoriesController < ApplicationController
 
+  before_action :set_story, only: [:show, :destroy]
+
+
   def index
+    @stories = Story.all
+    render json: @stories, status: 200
   end
+
+
+  def create
+    @story = Story.create(title: params["title"])
+    render json: @story, status: 201
+  end
+
+
+  def show
+    render json: @story, status: 200
+  end
+
+  def destroy
+    @story.destroy
+    render json: {}, status: 204 
+  end
+
+
+  private
+
+  def set_story
+   @story = Story.find_by(id: params[:id])
+  end
+
 
 end
