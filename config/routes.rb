c348172be0ebfa369e0a6fc47c80ea4d7eb6e004
LKWLaLa <lkwlala@gmail.com>
2016-07-26 21:01:04 -0400

    add routes for all models

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  
    root 'home#index'
+
+   resources :stories do
+    resources :contributions
+  end
+
+   resources :genres
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
