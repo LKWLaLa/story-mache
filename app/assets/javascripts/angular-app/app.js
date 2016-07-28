1eabbf73e70aa6bb029087bed8b3ad706079e1d5
LKWLaLa <lkwlala@gmail.com>
2016-07-27 23:08:08 -0400

    add templates module dependency

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,3 +1,13 @@
 angular
-    .module('app', ['ui.router'])
+    .module('app', ['ui.router', 'templates'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: 'landing.html'
+        });
+      
     
+    $urlRouterProvider.otherwise('/');
+   });
+  
