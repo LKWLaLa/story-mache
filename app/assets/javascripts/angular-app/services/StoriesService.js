3597329cb7fadeeb014e113f17ff81250379bede
LKWLaLa <lkwlala@gmail.com>
2016-07-28 18:15:28 -0400

    changed https to http in .get request

--- app/assets/javascripts/angular-app/services/StoriesService.js
+++ app/assets/javascripts/angular-app/services/StoriesService.js
@@ -1,10 +1,11 @@
 function StoriesService($http) {
   this.getStories = function () {
-    return $http.get('https://localhost:3000/stories')
+    return $http.get('http://localhost:3000/stories.json')
     .then(function(data){
       console.log(data);
     });
   };
+};
 
 
 angular
