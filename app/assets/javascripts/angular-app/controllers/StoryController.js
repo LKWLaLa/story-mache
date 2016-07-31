b9354fcbadc030d576dae8637daf3b47a38443b4
LKWLaLa <lkwlala@gmail.com>
2016-07-31 17:04:18 -0400

    add contributorCount and lastUpdate properties

--- app/assets/javascripts/angular-app/controllers/StoryController.js
+++ app/assets/javascripts/angular-app/controllers/StoryController.js
@@ -1,5 +1,10 @@
 function StoryController(story) {
-  this.data = story.data;
+  var ctrl = this
+
+  ctrl.data = story.data;
+  ctrl.contributorCount = story.data.contributions.length;
+  ctrl.lastUpdate = (story.data.contributions.pop()).created_at;
+
 }
 
 angular
