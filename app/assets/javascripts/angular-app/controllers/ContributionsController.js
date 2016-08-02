5ebe78645cb58d3072f4741a5a3d17471ac21228
LKWLaLa <lkwlala@gmail.com>
2016-08-02 17:36:04 -0400

    add contributions controller with submitForm function and author and body properties

---
+++ app/assets/javascripts/angular-app/controllers/ContributionsController.js
@@ -0,0 +1,22 @@
+function ContributionsController(ContributionsService, $stateParams) {
+    var ctrl = this;
+
+    ctrl.author = ''; 
+    ctrl.body = '';   
+
+ 
+    this.submitForm = function() {
+        ContributionsService
+            .saveContribution($stateParams.user_id, ctrl.author, ctrl.body)
+            .then(function () {
+                alert('successfully saved to database!');
+            }, function(error){
+        alert('Unable to submit: ' + error.statusText);;
+    };
+
+
+}
+
+angular
+  .module('app')
+  .controller('ContributionsController', ContributionsController);
