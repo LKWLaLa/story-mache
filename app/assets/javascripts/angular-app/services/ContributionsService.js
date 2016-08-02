96ef5f00ff75111dda3b48f2ac2f903bc76b8a9f
LKWLaLa <lkwlala@gmail.com>
2016-08-01 23:20:09 -0400

    create contributions service -  request not yet written

---
+++ app/assets/javascripts/angular-app/services/ContributionsService.js
@@ -0,0 +1,14 @@
+var API_URL ='http://localhost:3000'
+
+function ContributionsService($http) {
+  
+   this.saveContribution = function (story_id) {
+    
+  };
+};
+
+
+
+angular
+  .module('app')
+  .service('ContributionsService', ContributionsService);
