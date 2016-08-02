0bd2637cffedb9c19d53d16fa1c0664fbd6910b8
LKWLaLa <lkwlala@gmail.com>
2016-08-02 18:22:59 -0400

    fix route used in  post request

--- app/assets/javascripts/angular-app/services/ContributionsService.js
+++ app/assets/javascripts/angular-app/services/ContributionsService.js
@@ -3,7 +3,7 @@ var API_URL ='http://localhost:3000'
 function ContributionsService($http) {
   
    this.saveContribution = function (story_id, author, body) {
-     return $http.post(API_URL +'/contributions', {
+     return $http.post(API_URL + '/stories/' + story_id + '/contributions', {
       story_id: story_id, 
       author: author,
       body: body
