89301b3823deac72282457c00ace7cda27a0699e
LKWLaLa <lkwlala@gmail.com>
2016-08-03 18:09:09 -0400

    add createStory function

--- app/assets/javascripts/angular-app/controllers/NewStoryController.js
+++ app/assets/javascripts/angular-app/controllers/NewStoryController.js
@@ -1,5 +1,19 @@
-function NewStoryController(StoriesService) {
+function NewStoryController(StoriesService, $state) {
   var ctrl = this
+
+   ctrl.title = '';   
+
+ 
+    ctrl.createStory = function() {
+        StoriesService
+            .saveStory(ctrl.title)
+            .then(function (response) {
+               debugger;
+               $state.go('story', {id: response.data.id});
+            }, function(error){
+        alert('Unable to submit: ' + error.statusText);
+       })
+    }
  
 
 }
