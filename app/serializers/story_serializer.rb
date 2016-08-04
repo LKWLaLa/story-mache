1625fa912437ef3928988a2690409c0cef48adb9
LKWLaLa <lkwlala@gmail.com>
2016-08-03 21:26:50 -0400

    add new attributes :last_updated and :total_contributions to story serializer

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,14 +1,10 @@
 class StorySerializer < ActiveModel::Serializer
-  attributes :id, :title
+  attributes :id, :title, :last_updated, :total_contributions
 
   has_many :contributions
   class ContributionSerializer < ActiveModel::Serializer
     attributes :author, :body, :created_at
   end
 
-  has_many :genres
-  class GenreSerializer < ActiveModel::Serializer
-    attributes :name
-  end
 
 end
