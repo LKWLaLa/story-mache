edd4417411bef60f1495a2ea108649b73b7d8ac2
LKWLaLa <lkwlala@gmail.com>
2016-08-05 14:18:13 -0400

    add custom filter for SelectedOrder drop down menu, because Angular's orderBy does not work on an array of json objects

---
+++ app/assets/javascripts/angular-app/filters/SelectedOrder.js
@@ -0,0 +1,17 @@
+function SelectedOrder() {
+    return function(stories_objects, property, reverse) {
+    var filtered = [];
+    angular.forEach(stories_objects, function(story) {
+      filtered.push(story);
+    });
+    filtered.sort(function (a, b) {
+      return (a[property] > b[property] ? 1 : -1);
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+}
+ 
+angular
+    .module('app')
+    .filter('selectedOrder', SelectedOrder);
