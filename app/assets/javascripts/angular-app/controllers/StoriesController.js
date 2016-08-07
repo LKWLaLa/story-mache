a12dc921489fa871cf75ba2d02bb9c4320c76f18
LKWLaLa <lkwlala@gmail.com>
2016-08-07 16:20:20 -0400

    add methods for pagination of stories index state

--- app/assets/javascripts/angular-app/controllers/StoriesController.js
+++ app/assets/javascripts/angular-app/controllers/StoriesController.js
@@ -1,9 +1,34 @@
+var STORIES_PER_PAGE = 12;
+
 function StoriesController(stories) {
-  this.data = stories.data;
-  this.search = '';
-  this.sortBy = '';
-  this.minContributions = '';
-  this.maxContributions = '';
+  var ctrl = this;
+
+  ctrl.data = stories.data;
+  ctrl.search = '';
+  ctrl.sortBy = '';
+  ctrl.minContributions = '';
+  ctrl.maxContributions = ''; //end of search/filter bindings
+
+
+  ctrl.page = 0; //bindings for pagination
+  ctrl.totalStories = ctrl.data.length;
+  ctrl.totalPages = Math.ceil(ctrl.totalStories / STORIES_PER_PAGE);
+
+  ctrl.paginatePosts = function () {
+    ctrl.stories = ctrl.data.slice(ctrl.page * STORIES_PER_PAGE, (ctrl.page + 1) * STORIES_PER_PAGE);
+  };
+
+  ctrl.nextPage = function () {
+    ctrl.page++;
+    ctrl.paginatePosts()
+  };
+
+  ctrl.previousPage = function () {
+    ctrl.page--;
+    ctrl.paginatePosts();
+  };
+
+  ctrl.paginatePosts();
 }
 
 angular
