9b8b12947911988e2c2eba9c91416a383d6b32b7
LKWLaLa <lkwlala@gmail.com>
2016-08-07 15:53:54 -0400

    handle users typing an invalid id directly into the url.  Give error alert and redirect to index page

--- app/assets/javascripts/angular-app/controllers/StoryController.js
+++ app/assets/javascripts/angular-app/controllers/StoryController.js
@@ -1,10 +1,14 @@
-function StoryController(StoriesService, $stateParams) {
+function StoryController(StoriesService, $stateParams, $state) {
   var ctrl = this
   
 
   ctrl.getStory = function(){
     StoriesService.getStory($stateParams.id)
     .then(function(story){
+      if(story.data == null){
+        alert('That is not a valid story id.');
+        return $state.go('stories');
+      }
       ctrl.data = story.data;
       ctrl.contributions = story.data.contributions;
     }, function(error){
