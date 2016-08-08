2159dd08f78e0a6a1a506586a21a71ef8583c5ea
LKWLaLa <lkwlala@gmail.com>
2016-08-08 18:02:04 -0400

    move filtering into controller with  directive, and chain filters together.  Fix issue of pagination affecting filters, by filtering before paginating, then slicing the pre-filtered/ordered data to paginate.  Rename paginatePosts() to paginate()

--- app/assets/javascripts/angular-app/controllers/StoriesController.js
+++ app/assets/javascripts/angular-app/controllers/StoriesController.js
@@ -1,34 +1,51 @@
 var STORIES_PER_PAGE = 12;
 
-function StoriesController(stories) {
+function StoriesController(stories, $filter) {
   var ctrl = this;
 
   ctrl.data = stories.data;
   ctrl.search = '';
   ctrl.sortBy = '';
-  ctrl.minContributions = '';
-  ctrl.maxContributions = ''; //end of search/filter bindings
+  ctrl.minContributions = undefined;
+  ctrl.maxContributions = undefined; //end of search/filter bindings
 
 
-  ctrl.page = 0; //bindings for pagination
-  ctrl.totalStories = ctrl.data.length;
-  ctrl.totalPages = Math.ceil(ctrl.totalStories / STORIES_PER_PAGE);
+  ctrl.filteredList = stories.data; //filtering done in the controller
 
-  ctrl.paginatePosts = function () {
-    ctrl.stories = ctrl.data.slice(ctrl.page * STORIES_PER_PAGE, (ctrl.page + 1) * STORIES_PER_PAGE);
+   ctrl.refilter = function () {
+    var filtered = ctrl.data;
+    filtered = $filter('filter')(filtered, ctrl.search);
+    filtered = $filter('greaterThan')(filtered, ctrl.minContributions); 
+    filtered = $filter('lessThan')(filtered, ctrl.maxContributions); 
+    filtered = $filter('selectedOrder')(filtered, ctrl.sortBy, true);
+    ctrl.filteredList = filtered;
+    ctrl.resetPageData();
+    ctrl.paginate();
+   } 
+
+  ctrl.resetPageData = function() {
+    ctrl.page = 0; //pagination
+    ctrl.totalStories = ctrl.filteredList.length;
+    ctrl.totalPages = Math.ceil(ctrl.totalStories / STORIES_PER_PAGE);
+  };
+
+
+  ctrl.paginate = function () {
+    ctrl.stories = ctrl.filteredList.slice(ctrl.page * STORIES_PER_PAGE, (ctrl.page + 1) * STORIES_PER_PAGE);
   };
 
   ctrl.nextPage = function () {
     ctrl.page++;
-    ctrl.paginatePosts()
+    ctrl.paginate()
   };
 
   ctrl.previousPage = function () {
     ctrl.page--;
-    ctrl.paginatePosts();
+    ctrl.paginate();
   };
 
-  ctrl.paginatePosts();
+  ctrl.resetPageData();
+  ctrl.paginate();
 }
 
 angular
