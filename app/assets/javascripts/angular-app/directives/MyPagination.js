5c277ccdb896730c9614e02e2dd2da389a53e315
LKWLaLa <lkwlala@gmail.com>
2016-08-22 20:05:32 -0400

    add restrict E, scope with two way bindings, and copy over pagination methods from StoriesController (goal is to fully move pagination logic from the controller into a directive)

--- app/assets/javascripts/angular-app/directives/MyPagination.js
+++ app/assets/javascripts/angular-app/directives/MyPagination.js
@@ -1,6 +1,37 @@
 function MyPagination() {
     return {
-        templateUrl: 'directives/page-buttons.html'
+        templateUrl: 'directives/page-buttons.html',
+        restrict:  'E',
+
+        scope: {
+          currentPageNo: '=',
+          allItems: '=',
+          currentItems: '=',
+          itemsPerPage: '='
+        },
+
+        controller: function ($scope) {
+
+          $scope.previousPage = function () {
+            $scope.currentPageNo--;
+            $scope.paginate();
+          }
+
+          $scope.nextPage = function() {
+            $scope.currentPageNo++;
+            $scope.paginate();
+          }
+          
+          $scope.paginate = function () {
+            var begin = (($scope.currentPageNo - 1) * $scope.itemsPerPage);
+            var end = begin + $scope.itemsPerPage;
+            $scope.currentItems = $scope.allItems.slice(begin,end);
+          };
+
+          
+          
+          $scope.paginate();
+        }
     };
 }
  
