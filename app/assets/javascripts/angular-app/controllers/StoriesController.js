35376e5333c0f279a0a6f5d776068f86f324cb93
LKWLaLa <lkwlala@gmail.com>
2016-08-22 22:23:18 -0400

    rename filteredList to filteredStories, remove all pagination methods (moved logic to myPagination directive), add ctrl.displayedStories for use in ng-repeat (this is bound to the pagination directive), edit notes for clarity.

--- app/assets/javascripts/angular-app/controllers/StoriesController.js
+++ app/assets/javascripts/angular-app/controllers/StoriesController.js
@@ -1,16 +1,16 @@
-var STORIES_PER_PAGE = 12;
-
 function StoriesController(stories, $filter) {
   var ctrl = this;
 
   ctrl.data = stories.data;
+
   ctrl.search = '';
   ctrl.sortBy = '';
   ctrl.minContributions = undefined;
-  ctrl.maxContributions = undefined; //end of search/filter bindings
+  ctrl.maxContributions = undefined; //search/filter bindings
 
+  ctrl.filteredStories = stories.data; //default value for filtering in the controller
 
-  ctrl.filteredList = stories.data; //filtering done in the controller
+  ctrl.displayedStories = []; //two-way binding with myPagination directive
 
    ctrl.refilter = function () {
     var filtered = ctrl.data;
@@ -18,34 +18,8 @@ function StoriesController(stories, $filter) {
     filtered = $filter('greaterThan')(filtered, ctrl.minContributions); 
     filtered = $filter('lessThan')(filtered, ctrl.maxContributions); 
     filtered = $filter('selectedOrder')(filtered, ctrl.sortBy, true);
-    ctrl.filteredList = filtered;
-    ctrl.resetPageData();
-    ctrl.paginate();
-   } 
-
-  ctrl.resetPageData = function() {
-    ctrl.page = 0; //pagination
-    ctrl.totalStories = ctrl.filteredList.length;
-    ctrl.totalPages = Math.ceil(ctrl.totalStories / STORIES_PER_PAGE);
-  };
-
-
-  ctrl.paginate = function () {
-    ctrl.stories = ctrl.filteredList.slice(ctrl.page * STORIES_PER_PAGE, (ctrl.page + 1) * STORIES_PER_PAGE);
-  };
-
-  ctrl.nextPage = function () {
-    ctrl.page++;
-    ctrl.paginate()
-  };
-
-  ctrl.previousPage = function () {
-    ctrl.page--;
-    ctrl.paginate();
-  };
-
-  ctrl.resetPageData();
-  ctrl.paginate();
+    ctrl.filteredStories = filtered;
+   }  
 }
 
 angular
