878478f500cd8c038e2fb7f1f0a8ba2fa515c156
LKWLaLa <lkwlala@gmail.com>
2016-08-27 18:13:41 -0400

    add author_attributes setter method, for nested author association/creation

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,10 @@
 class Contribution < ActiveRecord::Base
   belongs_to :story
   belongs_to :author
+
+  def author_attributes=(attributes)
+    this_author = Author.find_or_create_by(attributes)
+    self.author = this_author
+  end
+
 end
