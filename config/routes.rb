47ea3f8bf336a3075f552a00434ca98744ffc4b4
LKWLaLa <lkwlala@gmail.com>
2016-08-27 17:48:31 -0400

    remove contributions controller and routes which are not needed.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   
    root 'home#index'
 
-   resources :stories do
-    resources :contributions, only: [:create]
-  end
-
+    resources :stories 
     resources :authors, only: [:create, :show]
 
 
