c579f0953e9ed0289e060605af71e175578746f8
LKWLaLa <lkwlala@gmail.com>
2016-08-27 19:00:13 -0400

    fix Author.find_by to include params[:name]

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,7 +2,7 @@ class AuthorsController < ApplicationController
 
 
   def show
-    @author = Author.find_by(:name)
+    @author = Author.find_by(name: params[:name])
     render json: @author, status: 200
   end
 
