63aa33eab641c7f5bb454a6477d64135b051e671
LKWLaLa <lkwlala@gmail.com>
2016-08-27 19:12:31 -0400

    add nested contributions to author json

--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -1,3 +1,9 @@
 class AuthorSerializer < ActiveModel::Serializer
   attributes :id, :name
+
+  has_many :contributions
+    class ContributionSerializer < ActiveModel::Serializer
+      attributes :body, :created_at, :story
+    end
+
 end
