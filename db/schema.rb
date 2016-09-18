0ead0256f44645751550bb72e471e89131ade325
LKWLaLa <lkwlala@gmail.com>
2016-09-17 22:18:04 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160827211446) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
